077193f37d5c7564cf8813effa99b5de8d70eb80
SiviR-13 <sivi.rakaj@icloud.com>
2024-12-17 18:37:19 -0500

    created warehouse route and indetified the endpoints

--- server.js
+++ server.js
@@ -5,3 +5,11 @@ import cors from 'cors';
 //Initializing Express APP
 const app = express();
 const PORT=process.env.PORT || 8080;
+
+//Middleware
+
+app.use(cors()); //To allow cross origin requests
+app.use(express.json());//To parse JSON request bodies
+
+//Import routes
+
