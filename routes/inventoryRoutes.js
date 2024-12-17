e77dc462daaba6d1f65f67116bbb5d761d1d7b3a
SiviR-13 <sivi.rakaj@icloud.com>
2024-12-17 18:40:31 -0500

    created warehouse route and identified the endpoints

---
+++ routes/inventoryRoutes.js
@@ -0,0 +1,26 @@
+import express from "express";
+const router = express.Router();
+
+//Creating routes for inventory
+router.get("/",(req,res)=>{
+    res.send('Get all inventory');
+});
+
+router.get("/:id",(req,res)=>{
+    res.send(`GET inventory with ID: ${req.params.id}`);
+});
+
+router.get("/:id/inventory", (req, res) => {
+    res.send(`GET inventory for inventory with ID: ${req.params.id}`);
+  });
+
+  router.put("/:id", (req, res) => {
+    res.send(`UPDATE inventory with ID: ${req.params.id}`);
+  });
+  router.delete("/:id", (req, res) => {
+    res.send(`DELETE inventory with ID: ${req.params.id}`);
+  });
+  router.post("/", (req, res) => {
+    res.send("CREATE a new inventory");
+  });
+  module.exports = router;
