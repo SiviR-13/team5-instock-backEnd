13d1452c3049a8b3b5893b226cbfd0fdb5f0a3b5
Khushi Parekh <khushi4541@gmail.com>
2024-12-18 14:19:13 -0500

    Added a controllers folder with a file for warehouses ctrlers and created the get request for warehouses data

--- knexfile.js
+++ knexfile.js
@@ -4,32 +4,18 @@ import "dotenv/config";
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 export default {
-  client: 'mysql2',
-  connection: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  }
-};
-// Import dotenv to process environment variables from `.env` file.
-import "dotenv/config";
-
-export default {
   client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    charset: "utf8",
   },
 
-migrations: {
-  directory: './migrations',
-},
-seeds: {
-  directory: './seeds',
-},
-}
-
+  migrations: {
+    directory: "./migrations",
+  },
+  seeds: {
+    directory: "./seeds",
+  },
+};
