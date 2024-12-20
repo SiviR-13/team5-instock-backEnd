b38b9fe2d794a7000a850368968c0a2a6104a7b1
SiviR-13 <sivi.rakaj@icloud.com>
2024-12-19 20:10:54 -0500

    updated inventory controller to fetch a single inventory item

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -5,26 +5,50 @@ const knex = initKnex(configuration);
 
 // Fetch all inventories
 export const getAllInventories = async (req, res) => {
-    try {
-      // Fetch inventories joined with warehouse names
-      const inventories = await knex("inventories")
-        .join("warehouses", "inventories.warehouse_id", "warehouses.id")
-        .select(
-          "inventories.id",
-          "warehouses.warehouse_name AS warehouse_name",
-          "inventories.item_name",
-          "inventories.description",
-          "inventories.category",
-          "inventories.status",
-          "inventories.quantity"
-        );
-  
-      res.status(200).json(inventories);
-    } catch (error) {
-      res.status(500).send(`Error fetching inventories: ${error.message}`);
-    }
-  };
+  try {
+    // Fetch inventories joined with warehouse names
+    const inventories = await knex("inventories")
+      .join("warehouses", "inventories.warehouse_id", "warehouses.id")
+      .select(
+        "inventories.id",
+        "warehouses.warehouse_name AS warehouse_name",
+        "inventories.item_name",
+        "inventories.description",
+        "inventories.category",
+        "inventories.status",
+        "inventories.quantity"
+      );
 
+    res.status(200).json(inventories);
+  } catch (error) {
+    res.status(500).send(`Error fetching inventories: ${error.message}`);
+  }
+};
+// Single inventory item by ID
+export const getInventoryById = async (req, res) => {
+  const inventoryId = req.params.id;
+  try {
+    const inventoryItem = await knex("inventories")
+      .join("warehouses", "inventories.warehouse_id", "warehouses.id")
+      .select(
+        "inventories.id",
+        "warehouses.warehouse_name AS warehouse_name",
+        "inventories.item_name",
+        "inventories.description",
+        "inventories.category",
+        "inventories.status",
+        "inventories.quantity"
+      )
+      .where("inventories.id", inventoryId)
+      .first();
+    if (!inventoryItem) {
+      return res.status(404).json({ message: "Inventory item not found" });
+    }
+    res.status(200).json(inventoryItem);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
 export const getInventoriesByWarehouseId = async (req, res) => {
   const warehouseId = req.params.id;
 
